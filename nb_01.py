2c1612fe51e6aae9c68e31c76d5e166c03e882d4
Andre Andrade Costa <andre.andrade.costa@gmail.com>
2020-01-27 16:33:26 -0300

    'nb_01'

---
+++ nb_01.py
@@ -0,0 +1,7 @@
+import operator
+
+def test(a, b, cmp, cname=None):
+    if cname is None: cname = cmp.__name__
+    assert cmp(a, b), f'{cname}:\n{a}\n{b}'
+
+def test_ep(a, b): test(a, b, operator.eq, '==')
